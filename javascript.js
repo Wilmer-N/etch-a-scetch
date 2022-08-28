1f9249d9b81867c9b0ae2e136499d8188c40451f
Wilmer <wilmernorling@gmail.com>
2022-08-28 21:42:25 +0200

    I now have the basic idea working where a mousehover will draw

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
 const container = document.querySelector(".container")
+const newDiv = document.createElement("div")
 for(let i = 0; i < 16**2; i++){   
     const newDiv = document.createElement("div")
-    newDiv.classList = ("black")
     container.appendChild(newDiv)
+    newDiv.addEventListener("mouseover", function(){
+        newDiv.classList = ("black")
+    })
 }
 
 
