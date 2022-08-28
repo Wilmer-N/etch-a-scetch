43d515b2c9a39bb1b6ac4046bbe38e712285c254
Wilmer <wilmernorling@gmail.com>
2022-08-28 22:09:15 +0200

    I now can change the width of the grid

--- javascript.js
+++ javascript.js
@@ -1,26 +1,22 @@
 const container = document.querySelector(".container")
-const newDiv = document.createElement("div")
 const resetBtn = document.querySelector(".reset")
 
-const width = 16
+const width = 40
+
+
 for(let i = 0; i < width**2; i++){   
+    createGrid();
+}
+
+function createGrid(){
     const newDiv = document.createElement("div")
     container.appendChild(newDiv)
     newDiv.addEventListener("mouseover", function(){
         newDiv.classList = ("black")
     })
-    resetBtn.addEventListener("click", ()=>{
-        newDiv.classList.remove("black")
-    })
-}
-
-
-
-
-
-
-
-
-
-
-
+    resetBtn.addEventListener("click", () =>{
+        newDiv.classList.remove("black")})
+    
+    newDiv.style.width = `calc(100% / ${width})`
+    newDiv.style.height = `calc(100% / ${width})`
+    }
