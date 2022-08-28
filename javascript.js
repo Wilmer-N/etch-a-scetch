baf116a73cdd5a8074df1cc6f73ca55c33b99871
Wilmer <wilmernorling@gmail.com>
2022-08-28 21:47:56 +0200

    Added a reset button

--- javascript.js
+++ javascript.js
@@ -1,12 +1,20 @@
 const container = document.querySelector(".container")
 const newDiv = document.createElement("div")
-for(let i = 0; i < 16**2; i++){   
+const resetBtn = document.querySelector(".reset")
+
+const width = 16
+for(let i = 0; i < width**2; i++){   
     const newDiv = document.createElement("div")
     container.appendChild(newDiv)
     newDiv.addEventListener("mouseover", function(){
         newDiv.classList = ("black")
     })
+    resetBtn.addEventListener("click", ()=>{
+        newDiv.classList.remove("black")
+    })
 }
+
+
 
 
 
