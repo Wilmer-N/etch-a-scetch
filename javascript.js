3c854e2389c14bec7ff6918dfe7820dc409348fa
Wilmer <wilmernorling@gmail.com>
2022-08-28 21:39:30 +0200

    I now have a container with js created divs

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
 const container = document.querySelector(".container")
-const newDiv = document.createElement("div")
+for(let i = 0; i < 16**2; i++){   
+    const newDiv = document.createElement("div")
+    newDiv.classList = ("black")
+    container.appendChild(newDiv)
+}
 
-container.appendChild(newDiv)
+
 
 
 
